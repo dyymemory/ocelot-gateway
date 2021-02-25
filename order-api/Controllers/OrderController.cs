d8b70b1f5e0ee7429a9ec38622c897daff6b3c02
dyymemory <38753272+dyymemory@users.noreply.github.com>
2021-02-25 14:43:51 +0800

    Initial commit

---
+++ order-api/Controllers/OrderController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace order_api.Controllers
+{
+    //[Route("api/[controller]/[action]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        // GET: api/Order
+        [Route("api/orders")]
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "订单1", "订单11" };
+        }
+        public IEnumerable<string> GetMore()
+        {
+            return new string[] { "订单2", "订单22" };
+        }
+    }
+}
